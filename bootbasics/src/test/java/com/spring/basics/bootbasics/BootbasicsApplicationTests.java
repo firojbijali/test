d7cc69b08038414250dbdcbdfe7c548417875416
firojbijali <bijalifiroj55@gmail.com>
2021-11-20 16:26:05 +0530

    unzip repository

---
+++ bootbasics/src/test/java/com/spring/basics/bootbasics/BootbasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basics.bootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootbasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
