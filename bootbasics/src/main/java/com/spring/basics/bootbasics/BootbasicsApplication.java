a64027424c3518f636ccf87b42491673c584ed5c
firojbijali <bijalifiroj55@gmail.com>
2021-11-20 16:44:22 +0530

    Update BootbasicsApplication.java

--- bootbasics/src/main/java/com/spring/basics/bootbasics/BootbasicsApplication.java
+++ bootbasics/src/main/java/com/spring/basics/bootbasics/BootbasicsApplication.java
@@ -1,13 +1,16 @@
 package com.spring.basics.bootbasics;
 
+
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootbasicsApplication {
-
-	public static void main(String[] args) {
+public class BootbasicsApplication 
+{
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(BootbasicsApplication.class, args);
-}
-
+	}
 }
